d4c063cb1b2d49cdc77f169f16433278563e0766
QueenaWang <User@MSI>
2019-10-25 19:46:54 +0800

    bugfix test
    新增bugfixServlet
    修改index

---
+++ Servlet/src/com/iii/eeit10931/basic/bugfixServlet.java
@@ -0,0 +1,32 @@
+package com.iii.eeit10931.basic;
+
+import java.io.IOException;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+
+@WebServlet("/bugfixServlet")
+public class bugfixServlet extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+
+    public bugfixServlet() {
+        super();
+       
+    }
+
+	
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+	
+		response.getWriter().append("bugfixServlet Served at: ").append(request.getContextPath());
+	}
+
+
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+	
+		doGet(request, response);
+	}
+
+}
