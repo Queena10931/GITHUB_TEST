af400c95daa2aa0c6ee1f5d5691b02bc8b1de9c8
QueenaWang <User@MSI>
2019-10-25 19:10:03 +0800

    二次修改
    嘗試看看有沒有錯誤

--- Servlet/src/com/iii/eeit10931/basic/HelloWorld.java
+++ Servlet/src/com/iii/eeit10931/basic/HelloWorld.java
@@ -22,7 +22,7 @@ public class HelloWorld extends HttpServlet {
 	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
 		
 		response.setContentType("text/html;charset=UTF-8");
-		response.getWriter().append("<html><body><h2>Hello everyone,到底為什麼會這麼難啊!!!</h2></body></html>").append(request.getContextPath());
+		response.getWriter().append("<html><body><h2>Hello everyone,在測試一次</h2></body></html>").append(request.getContextPath());
 	
 	}
 
