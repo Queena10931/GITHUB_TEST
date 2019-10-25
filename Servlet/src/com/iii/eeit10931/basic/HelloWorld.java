bc0d2e4f8c45765897595746462062aa17441288
QueenaWang <User@MSI>
2019-10-25 18:47:26 +0800

    HelloWord修改
    好難~~

--- Servlet/src/com/iii/eeit10931/basic/HelloWorld.java
+++ Servlet/src/com/iii/eeit10931/basic/HelloWorld.java
@@ -22,7 +22,7 @@ public class HelloWorld extends HttpServlet {
 	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
 		
 		response.setContentType("text/html;charset=UTF-8");
-		response.getWriter().append("<html><body><h2>Hello everyone,�j�a�n!!!</h2></body></html>").append(request.getContextPath());
+		response.getWriter().append("<html><body><h2>Hello everyone,到底為什麼會這麼難啊啊啊啊啊!!!</h2></body></html>").append(request.getContextPath());
 	
 	}
 
